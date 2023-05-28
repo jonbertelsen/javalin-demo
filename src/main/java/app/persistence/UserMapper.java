cc72e921b53a76bf884ba182c8d520b2bc68a3d1
Jon Bertelsen <jobe@cphbusiness.dk>
2023-05-28 17:09:32 +0200

    first commit

---
+++ src/main/java/app/persistence/UserMapper.java
@@ -0,0 +1,110 @@
+/**
+ * DB methods for user
+ */
+package app.persistence;
+
+import app.entities.User;
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class UserMapper
+{
+
+    static User login(String username, String password, ConnectionPool connectionPool) throws DatabaseException
+    {
+        Logger.getLogger("web").log(Level.INFO, "");
+
+        User user = null;
+
+        String sql = "SELECT * FROM user WHERE username = ? AND password = ?";
+
+        try (Connection connection = connectionPool.getConnection())
+        {
+            try (PreparedStatement ps = connection.prepareStatement(sql))
+            {
+                ps.setString(1, username);
+                ps.setString(2, password);
+                ResultSet rs = ps.executeQuery();
+                if (rs.next())
+                {
+                    String role = rs.getString("role");
+                    user = new User(username, password, role);
+                } else
+                {
+                    throw new DatabaseException("Wrong username or password");
+                }
+            }
+        } catch (SQLException ex)
+        {
+            throw new DatabaseException(ex, "Error logging in. Something went wrong with the database");
+        }
+        return user;
+    }
+
+    static User createUser(String username, String password, String role, ConnectionPool connectionPool) throws DatabaseException
+    {
+        Logger.getLogger("web").log(Level.INFO, "");
+        User user;
+        String sql = "insert into user (username, password, role) values (?,?,?)";
+        try (Connection connection = connectionPool.getConnection())
+        {
+            try (PreparedStatement ps = connection.prepareStatement(sql))
+            {
+                ps.setString(1, username);
+                ps.setString(2, password);
+                ps.setString(3, role);
+                int rowsAffected = ps.executeUpdate();
+                if (rowsAffected == 1)
+                {
+                    user = new User(username, password, role);
+                } else
+                {
+                    throw new DatabaseException("The user with username = " + username + " could not be inserted into the database");
+                }
+            }
+        }
+        catch (SQLException ex)
+        {
+            throw new DatabaseException(ex, "Could not insert username into database");
+        }
+        return user;
+    }
+
+    static List<User> getAllUsers(ConnectionPool connectionPool) throws DatabaseException
+    {
+        Logger.getLogger("web").log(Level.INFO, "");
+        List<User> userList = new ArrayList<>();
+        String sql = "select * from user";
+        try (Connection connection = connectionPool.getConnection())
+        {
+            try (PreparedStatement ps = connection.prepareStatement(sql))
+            {
+                ResultSet rs = ps.executeQuery();
+
+                while (rs.next())
+                {
+                    String userName = rs.getString("username");
+                    String password = rs.getString("password");
+                    String role = rs.getString("role");
+                    User user = new User(userName, password, role);
+                    userList.add(user);
+                }
+            }
+        }
+        catch (SQLException ex)
+        {
+            throw new DatabaseException(ex, "Could not get users from database");
+        }
+        return userList;
+    }
+
+
+}
