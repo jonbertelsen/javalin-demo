7a997c7fda24077fabded98195d4f93d084177d3
Jon Bertelsen <jobe@cphbusiness.dk>
2023-09-28 16:36:46 +0200

    add userfacadetest

--- src/test/java/app/model/persistence/UserFacadeTest.java
+++ src/test/java/app/model/persistence/UserFacadeTest.java
@@ -15,8 +15,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserFacadeTest {
 
-    private static final String USER = "dev";
-    private static final String PASSWORD = "ax2";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/startcode?currentSchema=test";
 
     private static ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL);
